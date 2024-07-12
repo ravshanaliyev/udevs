2bc340555f0fb14f5c86c2237d3ea80a0b899e5a
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-07-12 17:47:31 +0500

    optimizing code

--- src/components/shared/service-modal.tsx
+++ src/components/shared/service-modal.tsx
@@ -10,7 +10,7 @@ const ServiceModal = ({ bg, title, desc, img, modules, technos, hashtag, vertica
                         <div className="grid grid-cols-3 gap-x-2 gap-y-4 md:gap-5 mt-4 md:mt-8">
                             {
                                 modules.map((item: any, index: number) => (
-                                    <div key={index} className="flex flex-col items-start gap-3 bg-[#f4f7ff] p-4 md:p-5 rounded-lg">
+                                    <div key={index} className={`flex flex-col items-start gap-3 ${bg ? 'bg-[#e0e8ff]' : 'bg-[#f4f7ff]'} p-4 md:p-5 rounded-lg`}>
                                         <img className="w-[30px] h-[30px] md:w-[44px] md:h-[44px]" src={item.image} alt={item.name} />
                                         <p className="text-sm md:text-lg text-[#18191f] font-semibold truncate">{item.name}</p>
                                     </div>
@@ -24,7 +24,7 @@ const ServiceModal = ({ bg, title, desc, img, modules, technos, hashtag, vertica
                                     <div className="grid grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-4 md:gap-5 mt-4">
                                         {
                                             technos.map((item: any, index: number) => (
-                                                <div key={index} className="flex flex-col  items-center gap-1 bg-[#f4f7ff] p-2 rounded-lg">
+                                                <div key={index} className={`flex flex-col  items-center gap-1 ${bg ? 'bg-[#e0e8ff]' : 'bg-[#f4f7ff]'}  p-2 rounded-lg`}>
                                                     <img className="w-[30px] h-[30px] md:w-[50px] md:h-[50px]" src={item.image} alt={item.name} />
                                                     <p className="text-sm md:text-lg text-[#18191f] font-semibold">{item.name}</p>
                                                 </div>
